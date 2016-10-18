2cd9946261370fe703a96b2a24d565a7135436cc
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2016-10-18 01:42:57 -0700

    Revert "core: add option to disable openjdk check"
    This reverts commit bbed654567e59fd0ee0e1b62e334d319d8d4fd51.

--- core/main.mk
+++ core/main.mk
@@ -185,10 +185,8 @@ endif
 #
 # For Java 1.7, we require OpenJDK on linux and Oracle JDK on Mac OS.
 requires_openjdk := false
-ifeq ($(DISABLE_OPENJDK_CHECK),)
 ifeq ($(HOST_OS), linux)
 requires_openjdk := true
-endif
 endif
 
 
